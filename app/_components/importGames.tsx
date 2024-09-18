e8eaef2dde25b1a6a030292768a962ad888fb09b
SamuelBanfield <samueljbanfield@gmail.com>
2024-09-18 21:48:19 +0100

    Configure the source for import

--- app/_components/importGames.tsx
+++ app/_components/importGames.tsx
@@ -4,10 +4,11 @@ import React, { useState } from 'react';
 
 export default function ImportGames() {
   const [inputText, setInputText] = useState('');
+  const [importType, setImportType] = useState('chessdotcom');
 
   const importGames = async () => {
     try {
-      const response = await fetch(`/api/games/chessdotcom/${inputText}/import`, {
+      const response = await fetch(`/api/games/${importType}/${inputText}/import`, {
         method: 'POST',
       });
       const data = await response.json();
@@ -21,8 +22,16 @@ export default function ImportGames() {
   return (
       <div className="w-1/4">
         <div className="flex">
-          <input type="text" name="file" value={inputText} onChange={(e) => setInputText(e.target.value)} className="mr-2 px-2 py-1 border border-gray-300 rounded" />
+          <label>Username:</label>
+          <input type="text" value={inputText} onChange={(e) => setInputText(e.target.value)} className="px-2 py-1 border-gray-300 rounded" />
           <button onClick={importGames} className="px-2 py-2 bg-blue-500 text-white rounded">Import</button>
+        </div>
+        <div className="flex mt-2">
+          <label className="mr-2">Source:</label>
+          <select value={importType} onChange={(e) => setImportType(e.target.value)}>
+            <option value="chessdotcom">Chess.com</option>
+            <option value="lichess">Lichess</option>
+          </select>
         </div>
       </div>
     );
